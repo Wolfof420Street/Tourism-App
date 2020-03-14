7a0109e8183ff713ed86544f34b1aee224ebb402
Bill <billodida420@gmail.com>
2020-03-14 05:49:57 +0300

    setting up initial fields

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'screens/location_detail/locationdetail.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: LocationDetail(),);
+  }
+
+}
---
+++ lib/screens/location_detail/locationdetail.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/screens/location_detail/image_widget.dart';
+import 'text_section.dart';
+
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Column (
+
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      children:
+        [
+          ImageBanner("assets/images/wallpaper.jpg"),
+          TextSection(Colors.blue),
+          TextSection(Colors.red),
+          TextSection(Colors.green)
+    ],
+    )
+
+    );
+  }
+
+}
